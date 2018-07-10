5edd554d90eca164f35a06b6685188c7ab0b0887
Shabnam Esmaeili <esmaeili.shabnam@gmail.com>
2018-07-10 17:40:06 +0430

    update native express fragment

--- app/src/main/java/com/magnetadservices/magnetadsample/fragments/NativeExpressFragment.java
+++ app/src/main/java/com/magnetadservices/magnetadsample/fragments/NativeExpressFragment.java
@@ -1,14 +1,16 @@
 package com.magnetadservices.magnetadsample.fragments;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.magnetadservices.magnetadsample.R;
@@ -24,37 +26,18 @@ public class NativeExpressFragment extends Fragment {
 
     FrameLayout frNative;
     Button btnNative;
-    RelativeLayout nativeLayout;
+    private MagnetNativeExpress magnetNativeExpress;
+    private DisplayMetrics metrics;
     private int width;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab3_fragment, container, false);
-        nativeLayout = (RelativeLayout) View.inflate(getContext(), R.layout.native_content, null);
         frNative = view.findViewById(R.id.frNative);
         btnNative = view.findViewById(R.id.btnNative);
+        metrics = getResources().getDisplayMetrics();
 
-        width = (int) (getResources().getDisplayMetrics().widthPixels / getResources().getDisplayMetrics().density);
-        return view;
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        btnNative.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showNativeAd();
-            }
-        });
-    }
-
-    private void showNativeAd() {
-        if (nativeLayout.getParent() != null) {
-            ((ViewGroup) nativeLayout.getParent()).removeView(nativeLayout);
-        }
-
-        MagnetNativeExpress magnetNativeExpress = MagnetNativeExpress.create(getContext());
+        magnetNativeExpress = MagnetNativeExpress.create(getContext());
         magnetNativeExpress.setAdLoadListener(new MagnetAdLoadListener() {
             @Override
             public void onPreload(int i, String s) {
@@ -76,6 +59,34 @@ public class NativeExpressFragment extends Fragment {
 
             }
         });
+        ViewTreeObserver vto = frNative.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                    frNative.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                } else {
+                    frNative.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
+                width = (int) (frNative.getMeasuredWidth() / metrics.density);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        btnNative.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showNativeAd();
+            }
+        });
+    }
+
+    private void showNativeAd() {
         // TODO: replace the adUnitId with yours
         magnetNativeExpress.load("2f84c1f29e0408d5b305f98aad0e008e", frNative, new AdSize(width, width));
     }
